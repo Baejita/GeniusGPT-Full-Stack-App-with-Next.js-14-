ad054aa3b49cf84806e37840850ec8ca5c178b04
Baejita <homokoro@gmail.com>
2024-08-07 22:21:06 +0700

    Start prisma

--- my-next.js-app/app/prisma-example/page.js
+++ my-next.js-app/app/prisma-example/page.js
@@ -1,7 +1,29 @@
-const PrismaExample = () => {
+import prisma from "@/utils/db";
+
+const prismaHandler = async () => {
+  await prisma.task.create({
+    data: {
+      content: "Wake up",
+    },
+  });
+  const allTasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return allTasks;
+};
+
+const PrismaExample = async () => {
+  const tasks = await prismaHandler();
   return (
     <div>
       <h1 className="text-7xl">PrismaExample</h1>
+      <div>
+        {tasks.map((task) => {
+          return <h2 key={task.id}>{task.content}</h2>;
+        })}
+      </div>
     </div>
   );
 };
