d574e05fe084d492215a867909328a083a1931ab
Baejita <homokoro@gmail.com>
2024-08-08 23:49:06 +0700

    error message

--- my-next.js-app/app/lib/apiFetchDrink.js
+++ my-next.js-app/app/lib/apiFetchDrink.js
@@ -1,7 +1,7 @@
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 
 export default async function fetchDrinks() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   const response = await fetch(url);
   //Throw an error
 
--- my-next.js-app/utils/actions.js
+++ my-next.js-app/utils/actions.js
@@ -1,6 +1,7 @@
 "use server";
 import prisma from "@/utils/db";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const getAllTasks = async () => {
   return await prisma.task.findMany({
@@ -21,9 +22,43 @@ export const createTask = async (formData) => {
   revalidatePath("/tasks");
 };
 
+export const createTaskCustom = async (prevState, formData) => {
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  try {
+    const content = formData.get("content");
+
+    await prisma.task.create({
+      data: {
+        content,
+      },
+    });
+    revalidatePath("/tasks");
+    return { message: "😁success!!!" };
+  } catch (error) {
+    return { message: "😫error..." };
+  }
+};
+
 //DELETE
 export const deleteTask = async (formData) => {
   const id = formData.get("id");
   await prisma.task.delete({ where: { id } });
   revalidatePath("/tasks");
+};
+
+//Edit
+export const getTask = async (id) => {
+  return prisma.task.findUnique({
+    where: { id },
+  });
+};
+export const editTask = async (formData) => {
+  const id = formData.get("id");
+  const content = formData.get("content");
+  const completed = formData.get("completed");
+  await prisma.task.update({
+    where: { id },
+    data: { content, completed: completed === "on" ? true : false },
+  });
+  redirect("/tasks");
 };
