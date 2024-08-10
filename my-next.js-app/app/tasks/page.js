cd7b4018c6b6cb0ed874ac4fcc3f119674f1f0d8
Baejita <homokoro@gmail.com>
2024-08-10 22:15:02 +0700

    finish

--- my-next.js-app/app/tasks/page.js
+++ my-next.js-app/app/tasks/page.js
@@ -1,6 +1,7 @@
 import TaskFormCustom from "@/components/TaskFormCustom";
 import TaskList from "@/components/TaskList";
 
+export const dynamic = "force-dynamic";
 const Tasks = () => {
   return (
     <div className="max-w-lg">
